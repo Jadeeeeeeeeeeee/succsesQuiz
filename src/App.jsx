e1a6c6e4acfac0b020d743e0007f553d0d1a63b2
jadeeeeeeeeeeee <yali.shlomi@icloud.com>
2024-02-22 21:59:47 +0200

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import MainConnector from "./mainQconnector";
+import { Analytics } from "@vercel/analytics/react"
 function App() {
 
   return(<>
   <MainConnector/>
+  <Analytics></Analytics>
   </>);
 }
 
